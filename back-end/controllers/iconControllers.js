01bc509a925b32268afd59086f6119f944ad0d1c
Thomas-bon <pro.bonanseathomas@gmail.com>
2025-10-16 11:43:16 +0200

    feat: add category model and implement category routes with controllers

--- back-end/controllers/iconControllers.js
+++ back-end/controllers/iconControllers.js
@@ -1,9 +1,10 @@
 import { Icon } from "../models/icons.js";
+import { Category } from "../models/category.js";
 
 // Récupérer toutes les icones
 export async function getAllIcons(req, res) {
     try {
-        const icons = await Icon.find();    
+        const icons = await Icon.find();
         res.json(icons);
         console.log("✅ Récupération de toutes les icones réussie");
     } catch (error) {
@@ -12,16 +13,17 @@ export async function getAllIcons(req, res) {
     }
 }
 
+// Récupérer une icone par son ID
 export async function getIconById(req, res) {
     try {
-        const icon = new Icon.findById(req.params.id)
+        const icon = await Icon.findById(req.params.id)
         res.json(icon);
         console.log("✅ Récupération de toutes les icones réussie");
     } catch (error) {
         console.error("❌ Erreur lors de la récupération des icones : ", error);
         res.status(500).json({ message: error.message });
     }
-    
+
 }
 
 
@@ -38,3 +40,61 @@ export async function createIcon(req, res) {
     }
 }
 
+
+export async function updateIcon(req, res) {
+    try {
+        const icon = await Icon.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!icon) {
+            console.error("❌ Erreur lors de la mise à jour de l'icon : ", error);
+            return res.status(404).json({ message: "😔 Icone non trouvée.." })
+        }
+
+        res.json(icon)
+        console.log("✅ Icon mise à jour avec succès : ", icon);
+    } catch (error) {
+        console.error("❌ Erreur lors de la mise à jour de l'icon : ", error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+// Supprimer une icone
+// export async function deleteIcon(req, res) {
+//     try {
+//         const icon = await Icon.findByIdAndDelete(req.params.id);
+
+//         if (!icon) {
+//             return res.status(404).json({ message: "Icone non trouvée." });
+//         }
+
+//         res.json({ message: "✅ Icone supprimée avec succès.", icon });
+//     } catch (error) {
+//         console.error("❌ Erreur lors de la suppression de l'icone : ", error);
+        // res.status(500).json({ message: error.message });
+//     }
+// }
+
+// Récupérer toutes les catégories
+export async function getAllCategories(req, res) {
+    try {
+        const category = await Category.find()
+        res.json(category)
+        console.log("✅ Récupération de toutes les catégories réussie");
+    } catch (error) {
+        console.error("❌ Erreur lors de la récupération des catégories : ", error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+// Récupérer une catégorie par son ID
+export async function getCategoryById(req, res) {
+    try {
+        const category = await Category.findById(req.params.id)
+        res.json(category);
+        console.log("✅ Récupération de toutes les catégories réussie");
+    } catch (error) {
+        console.error("❌ Erreur lors de la récupération des catégories : ", error);
+        res.status(500).json({ message: error.message });
+    }
+
+}
--- back-end/models/icons.js
+++ back-end/models/icons.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const iconSchema = new mongoose.Schema({
-    name: { type: String, required: true, trim: true },
-    svg: { type: String, required: true },
-    tags: { type: [String], required: true }
+    "name": { type: String, required: true, trim: true },
+    "svg": { type: String, required: true },
+    "category": { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
 });
 
 export const Icon = mongoose.model("Icon", iconSchema);
