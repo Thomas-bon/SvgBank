d60e803cddeb69d869eeb37f4dca2e965d28b4e3
Thomas Richard <Th.RICHARD2@outlook.com>
2025-10-17 15:34:57 +0200

    added code preview of icons and added global style

--- front-end/svgbank/src/components/modal.vue
+++ front-end/svgbank/src/components/modal.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue';
-
 const props = defineProps({
   icon: {
     type: Object,
@@ -20,13 +18,26 @@ const closeModal = () => emit('close');
   <teleport to="body">
     <div
       v-if="show"
-      class="fixed inset-0 bg-[rgba(0,0,0,0.5)] bg-opacity-50 flex items-center justify-center z-50"
+      class="fixed inset-0 bg-[rgba(24,49,83,0.5)] flex items-center justify-center z-50"
     >
-      <div class="bg-white rounded-lg shadow-lg max-w-3xl w-full h-100 flex relative">
+      <div class="bg-white rounded-2xl max-w-4xl w-full h-96 flex relative overflow-hidden">
         <button @click="closeModal" class="absolute top-2 right-3 text-gray-500 text-2xl">&times;</button>
-
-        <div class="flex-1 p-8 flex items-center justify-center border-r">
+        <div class="flex-1 flex items-center justify-center m-8 border-2 border-[rgba(195,198,209,0.5)] rounded-2xl">
           <div class="w-48 h-48" v-html="icon.svg"></div>
+        </div>
+
+        <div class="flex-1 flex flex-col p-8">
+          <h2 class="text-lg font-semibold mb-2">{{ icon.name }}</h2>
+          <div class="flex-1 overflow-auto bg-[rgb(24,49,83)] border rounded-lg p-3 text-sm whitespace-pre-wrap !text-white !font-mono">
+            {{ icon.svg }}
+          </div>
+
+          <button
+            @click="navigator.clipboard.writeText(icon.svg)"
+            class="mt-3 w-full px-4 py-2 bg-[rgb(255,212,59)] hover:bg-[rgb(250,176,5)] border-2 border-b-8 border-[rgb(24,49,83)] rounded-2xl cursor-pointer"
+          >
+            Copy SVG
+          </button>
         </div>
       </div>
     </div>
--- front-end/svgbank/src/components/viewbox.vue
+++ front-end/svgbank/src/components/viewbox.vue
@@ -15,12 +15,12 @@ function handleClick() {
 
 <template>
   <div
-    class="bg-white rounded-lg p-4 flex flex-col items-center text-center shadow-sm hover:shadow-lg transition-transform transform cursor-pointer"
+    class="bg-white hover:bg-[rgb(255,212,59)] transition rounded-lg p-4 flex flex-col items-center text-center cursor-pointer"
     @click="handleClick"
   >
     <div class="w-14 h-14 mb-3 flex items-center justify-center">
       <div class="w-full h-full" v-html="icon.svg"></div>
     </div>
-    <div class="text-sm text-gray-700 truncate w-full">{{ icon.name }}</div>
+    <div class="text-sm truncate w-full">{{ icon.name }}</div>
   </div>
 </template>
